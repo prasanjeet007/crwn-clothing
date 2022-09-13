8174185da78cfa301b393f1d1df57ee8bf359bbe
Prasanjeet chatterjee <prasanjeetchatterjee@Prasanjeets-MacBook-Pro.local>
2022-09-13 12:23:00 +0530

    add header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/Header';
 import Homepage from './pages/homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Outlet/>
     </div>
   );
---
+++ src/components/header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import {ReactComponent as Logo}  from '../../assets/images/crown.svg';
+import './header.scss';
+export default function Header() {
+    return (
+        <div className='header'>
+            <Link to="/">
+             <Logo className='logo'/>
+            </Link>
+            <div className='options'>
+             <Link className='' to="/shop">
+             <span className='option'>SHOP</span>
+             </Link>
+             <Link className='' to="/shop">
+                <span className='option'>CONTACT</span>
+             </Link>
+            </div>
+        </div>
+    )
+}
